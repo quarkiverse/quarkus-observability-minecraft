b372c0c01c1cca022034b7969155ff117db39b96
Holly Cummins <holly.k.cummins@googlemail.com>
2026-04-21 17:53:54 +0100

    Move to latest Quarkus LTS, tidy up some dependencies

--- sample/src/test/java/io/quarkus/sample/TodoResourceTest.java
+++ sample/src/test/java/io/quarkus/sample/TodoResourceTest.java
@@ -1,10 +1,8 @@
 package io.quarkus.sample;
 
-import static io.restassured.RestAssured.delete;
-import static io.restassured.RestAssured.get;
-import static io.restassured.RestAssured.given;
-import static junit.framework.TestCase.assertEquals;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.List;
 
